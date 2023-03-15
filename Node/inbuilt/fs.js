ceba85e3aac4daf0d28e4df261fd5496bb1d048e
Shrishti-2 <shrishtishiv99@gmail.com>
2023-03-15 10:22:57 +0530

    fs

--- Node/inbuilt/fs.js
+++ Node/inbuilt/fs.js
@@ -1,9 +1,2 @@
 let fs = require('fs');
-//append file
-// fs.appendFile('MyFile.txt','My append code',function(){
-// })
-//read file 
-// fs.readFile('.../product.json','utf-8',function(err,data){
-//     if(err) throw err;
-//     console.log(data);
-// })
+
