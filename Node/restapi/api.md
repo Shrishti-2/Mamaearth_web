9a19677da609bbd7957adf34d17f2cd067c1b267
Shrishti-2 <shrishtishiv99@gmail.com>
2023-03-18 12:24:08 +0530

    api doc

---
+++ Node/restapi/api.md
@@ -0,0 +1,11 @@
+# Page 1
+> list of category 
+> list of subcategory
+> category wrt subcategory
+# Page 2
+
+# Page 3
+
+# Page 4
+
+# Page 5
--- Node/restapi/app.js
+++ Node/restapi/app.js
@@ -4,7 +4,8 @@ let port = 9310;
 let cors = require('cors');
 let mongo = require('mongodb');
 let MongoClient = mongo.MongoClient;
-let mongoUrl ="mongodb://localhost:27017";
+let bodyParser = require('body-parser')
+let mongoUrl = "mongodb://localhost:27017";
 let db;
 
 app.use(cors())
@@ -12,21 +13,21 @@ app.use(cors())
 app.get('/',(req,res) =>{
     res.send('<h1>Hiiii from express</h1>');
 })
-app.get('/location',(req,res) =>{
-    res.send('<h1>Hiiii from location</h1>');
+// get all category
+app.get('/category',(req,res) => {
+    db.collection('category').find().toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
 })
 
 //Connection with db
 MongoClient.connect(mongoUrl,(err,client) => {
     if(err) console.log('Error While Connecting');
-    db = client.db('') // database name
+    db = client.db('Mamaearth') // database name
     app.listen(port,(err) => {
         if(err) throw err;
         console.log(`Server is running on port ${port}`)
     })
 })
  
-app.listen(port,(err)=>{
-    if (err) throw err;
-     console.log(`Server is running at port ${port}`)
-})
